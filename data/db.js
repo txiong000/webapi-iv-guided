618a05a905fc0af9a8c7d49a6d1b29fd97f66078
txiong000 <tkx422@gmail.com>
2019-04-24 20:35:30 -0500

    heroku deployment

--- data/db.js
+++ data/db.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
 const knex = require('knex');
 
-const config = require('../knexfile.js').development;
+const db = process.env.DB_ENV || 'production';
+
+
+const config = require('../knexfile.js')[db];
 
 module.exports = knex(config);
